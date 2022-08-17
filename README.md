7e2520726e31d9ee71784fee306bf3e937635f14
Othman Aladlan <Othman.aladlan96@gmail.com>
2022-08-17 21:36:29 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,136 @@
+# Sublime Laravel Pint Fixer
+This is a plugin for Sublime Text 3/4, to format PHP code through laravel pint command on any view.
+
+ported from the Awesome SublimePhpCsFixer package of [@adael](https://github.com/adael)
+
+# Features
+
+* It works inside a temporal view (ie: on an new, non-saved file)
+* Fast
+* Easy
+* Configurable through config file
+* Tested on Windows, Linux, and Mac
+
+# Configuration
+
+You have to install `laravel/pint` (the actual package made by Laravel, not this plugin)
+
+You can install `laravel/pint` package directly with composer by running:
+
+    composer global require laravel/pint
+
+Also you can create a config file as explained here https://laravel.com/docs/pint#configuring-pint
+
+*for example in:* `$HOME/pint.json`
+
+```js
+{
+    "preset": "laravel",
+    "rules": {
+        "simplified_null_return": true,
+        "braces": false,
+        "new_with_braces": {
+            "anonymous_class": false,
+            "named_class": false
+        }
+    }
+}
+```
+
+If you've created a config file, you have to configure its path in the plugin's settings.
+
+*In Menu -> Preferences -> Package Settings -> Laravel Pint Fixer -> Settings - user*
+
+```js
+{
+    "config": "/path/to/pint.json"
+}
+```
+
+When using multiple projects with different configurations, it's possible to
+configure the path relative to the Sublime project folder:
+
+```js
+{
+    "config": "${folder}/pint.json",
+    "php": "${packages}/User/php",
+    "path": "${packages}/User/pint"
+}
+```
+
+It's also possible to specify multiple config paths. In that case, the first readable file is used:
+
+```js
+{
+    "config": [
+        "${file_path}/pint.json",
+        "${folder}/pint.json",
+        "/path/to/pint.json"
+    ]
+}
+```
+
+See [`extract_variables` in the Sublime API Reference](https://www.sublimetext.com/docs/3/api_reference.html#sublime.Window)
+for the supported replacement variables. The value of the `${folder}` points
+the path of the first project in Sublime API. Here, it's beforehand replaced
+with the path of the project the target file belongs.
+
+Please note:
+
+* **`config` directive is excluding.**
+* This plugin don't try to find the config file automatically. If you want to
+create a config file, you have to specify its path in the plugin settings.
+
+For more information see: https://github.com/FriendsOfPHP/PHP-CS-Fixer#usage
+
+## Exclude files
+
+Since all php files are passed directly to the laravel pint executable, a configured PhpCsFixer\\Finder gets ignored.
+In order to exclude files from pint, you can use the "exclude" setting:
+
+```js
+{
+    "exclude": [
+        ".*[\\\\/]vendor[\\\\/].*", // vendor-path (used by Composer)
+        ".*\\.phtml$" // files ending with ".phtml"
+    ]
+}
+```
+
+The exclude-filter uses python regular expressions.
+For more information see: https://docs.python.org/2/library/re.html
+
+
+### On Windows:
+
+The plugin tries to find the executable in:
+
+    %APPDATA%\composer\vendor\bin\pint.bat
+
+If it isn't working, you can locate your composer global packages path by running:
+
+    composer config -g home
+
+### On Linux:
+
+After installing larave/pint you have to specify the full path to the
+executable in the configuration page.
+
+The plugin tries to find the executable in:
+
+    $HOME/.composer/vendor/bin/pint
+
+However, if it isn't working, you can create a symbolic link to the pint executable
+
+    ln -s $HOME/.composer/vendor/bin/pint $HOME/bin/pint
+
+
+# Acknowledgements
+
+I would like to thank to adael, sensiolabs and contributors for their awesome package.
+It works flawlessly. All the work here belongs to them.
+
+Check them at:
+* https://github.com/adael
+* https://github.com/FriendsOfPHP/PHP-CS-Fixer
+* http://cs.sensiolabs.org/
---
+++ SublimeLaravelPintFixer.py
@@ -0,0 +1,346 @@
+import sublime
+import sublime_plugin
+import os
+import tempfile
+import subprocess
+import re
+import json
+
+
+def load_settings():
+    return sublime.load_settings("SublimeLaravelPintFixer.sublime-settings")
+
+
+def setting_enabled(name):
+    return load_settings().get(name)
+
+
+def is_windows():
+    return sublime.platform() == "windows"
+
+
+def is_executable_file(file_path):
+    return os.path.isfile(file_path) and os.access(file_path, os.X_OK)
+
+
+def is_readable_file(file_path):
+    return os.path.isfile(file_path) and os.access(file_path, os.R_OK)
+
+
+def which(program):
+    """Code from: https://stackoverflow.com/a/377028/584639"""
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_executable_file(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_executable_file(exe_file):
+                return exe_file
+
+    return None
+
+
+def pint_possible_paths():
+    paths = []
+
+    if is_windows():
+        executable_name = "pint.bat"
+    else:
+        executable_name = "pint"
+
+    project_data = sublime.active_window().project_data()
+    project_path = project_data['folders'][0]['path']
+    paths.append(os.path.join(project_path, "vendor", "bin", executable_name))
+
+    if "COMPOSER_HOME" in os.environ:
+        paths.append(os.path.join(
+            os.environ["COMPOSER_HOME"], "vendor", "bin", executable_name))
+
+    if "APPDATA" in os.environ:
+        paths.append(os.path.join(
+            os.environ["APPDATA"], "composer", "bin", executable_name))
+
+    if "HOME" in os.environ:
+        paths.append(os.path.join(
+            os.environ["HOME"],
+            ".composer",
+            "vendor",
+            "bin",
+            executable_name))
+
+        paths.append(os.path.join(
+            os.environ["HOME"],
+            ".config",
+            "composer",
+            "vendor",
+            "bin",
+            executable_name))
+
+    paths.append(which(executable_name))
+
+    return paths
+
+
+def create_process_for_platform(cmd):
+    if is_windows():
+        # We need to hide the console window
+        si = subprocess.STARTUPINFO()
+        si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+    else:
+        si = None
+
+    return subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        bufsize=0,
+        startupinfo=si)
+
+
+def get_project_folder(file):
+
+    project_data = sublime.active_window().project_data()
+
+    if not project_data:
+        return None
+
+    project_folders = project_data.get('folders', [])
+    project_paths = (p['path'] for p in project_folders)
+    for path in project_paths:
+        if file.startswith(path):
+            return path
+
+    return None
+
+
+class Logger:
+    def __init__(self, settings):
+        self.settings = settings
+
+    def console(self, msg):
+        print("Larave Pint Fixer: {0}".format(msg))
+
+    def debug(self, msg):
+        if self.settings.get("debug"):
+            print("Larave Pint Fixer (DEBUG): {0}".format(msg))
+
+
+class FormatterSettings:
+    def __init__(self, settings):
+        self.settings = settings
+        self.variables = self.get_active_window_variables()
+
+    def get(self, key):
+        return self.settings.get(key)
+
+    def get_expanded(self, key):
+        return self.expand(self.settings.get(key))
+
+    def expand(self, value):
+        return sublime.expand_variables(value, self.variables)
+
+    def get_active_window_variables(self):
+        variables = sublime.active_window().extract_variables()
+
+        if 'file' in variables:
+            folder = get_project_folder(variables['file'])
+            if folder:
+                variables['folder'] = folder
+
+        return variables
+
+
+class FixerProcess:
+    def __init__(self, settings: FormatterSettings, logger: Logger):
+        self.logger = logger
+        self.settings = settings
+
+    def run(self, tmp_file):
+        cmd = self.create_cmd(tmp_file)
+        self.logger.debug("Using cmd:")
+        self.logger.debug(cmd)
+
+        p = create_process_for_platform(cmd)
+        output, err = p.communicate()
+
+        if p.returncode != 0:
+            self.logger.console("There was an error formatting the view")
+            self.logger.console("Command: {0}".format(cmd))
+            self.logger.console("Error output: {0}".format(err))
+
+    def create_cmd(self, tmp_file):
+        config = self.config_param()
+
+        return list(filter(None, [
+            self.settings.get_expanded('php_path'),
+            self.get_configured_laravel_pint_path(),
+            config,
+            "--using-cache=no",
+            tmp_file,
+        ]))
+
+    def config_param(self):
+        configs = self.settings.get("config")
+
+        if not configs:
+            return None
+
+        if not type(configs) is list:
+            configs = [configs]
+
+        for config in configs:
+            config_path = self.settings.expand(config)
+            self.logger.debug("Looking for config: " + config_path)
+            if is_readable_file(config_path):
+                self.logger.console("Using config: " + config_path)
+                return '--config=' + config_path
+
+        self.logger.debug("Not using config")
+
+        return None
+
+    def get_configured_laravel_pint_path(self):
+        path = self.settings.get_expanded('path')
+
+        if not path:
+            path = self.locate_laravel_pint()
+
+        if not path:
+            raise ExecutableNotFoundException("Couldn't find laravel pint")
+
+        if not is_executable_file(path):
+            raise ExecutableNotFoundException(
+                "Couldn't execute file: {0}".format(path))
+
+        return path
+
+    def locate_laravel_pint(self):
+        paths = pint_possible_paths()
+        for path in paths:
+            self.logger.debug("looking for laravel pint at: " + path)
+            if is_executable_file(path):
+                self.logger.console("autodetected: " + path)
+                return path
+
+        self.logger.console("laravel pint file not found")
+
+
+class ViewFormatter:
+
+    def __init__(self, settings: FormatterSettings, logger):
+        self.settings = settings
+        self.logger = logger
+
+    def format(self, contents):
+        self.logger.console("Formatting view...")
+        return self.format_contents(contents)
+
+    def format_contents(self, contents):
+        """
+        Write the contents in a temporary file, format it with laravel pint and returns the formatted contents.
+
+        For supporting ST2 and ST3, I do use the following, because it's compatible in python 2/3 and seems
+        to work properly.
+
+            - file.write(contents.encode(encoding))
+            - file.read().decode(encoding)
+
+        :param contents:
+        :return:
+        """
+        fd, tmp_file = tempfile.mkstemp()
+
+        encoding = self.settings.get("encoding")
+
+        with open(tmp_file, 'wb') as file:
+            file.write(contents.encode(encoding))
+            file.close()
+
+        try:
+            self.format_file(tmp_file)
+            with open(tmp_file, 'rb') as file:
+                content = file.read().decode(encoding)
+                file.close()
+        finally:
+            os.close(fd)
+            os.remove(tmp_file)
+
+        return content
+
+    def format_file(self, tmp_file):
+        fixer = FixerProcess(self.settings, self.logger)
+        fixer.run(tmp_file)
+
+
+class SublimePhpCsFixCommand(sublime_plugin.TextCommand):
+
+    def __init__(self, view):
+        sublime_plugin.TextCommand.__init__(self, view)
+        self.settings = load_settings()
+        self.logger = Logger(self.settings)
+
+    def is_enabled(self):
+        return self.is_supported_scope()
+
+    def run(self, edit):
+        try:
+            self.format(edit)
+        except ExecutableNotFoundException as e:
+            self.logger.console(str(e))
+
+    def format(self, edit):
+        region = sublime.Region(0, self.view.size())
+        contents = self.view.substr(region)
+
+        if not contents:
+            self.logger.console("Done. No contents")
+            return
+
+        formatter = ViewFormatter(
+            FormatterSettings(self.settings), self.logger)
+        new_contents = formatter.format(contents)
+
+        if new_contents and new_contents != contents:
+            self.view.replace(edit, region, new_contents)
+            self.logger.console("Done. View formatted")
+        else:
+            self.logger.console("Done. No changes")
+
+    def is_supported_scope(self):
+        scopes = self.view.scope_name(self.view.sel()[0].begin())
+        return 'embedding.php' in scopes and not self.is_excluded()
+
+    def is_excluded(self):
+        if not self.settings.has('exclude'):
+            return False
+
+        exclude = self.settings.get('exclude')
+        file_name = self.view.file_name()
+
+        if not type(exclude) is list:
+            exclude = [exclude]
+
+        for pattern in exclude:
+            if re.match(pattern, file_name) is not None:
+                self.logger.console(
+                    file_name + ' is excluded via pattern: ' + pattern)
+                return True
+
+        return False
+
+
+class SublimeLaravelPintFixListener(sublime_plugin.EventListener):
+    def on_pre_save(self, view):
+        if setting_enabled('on_save'):
+            view.run_command('sublime_laravel_pint_fix')
+
+    def on_load(self, view):
+        if setting_enabled('on_load'):
+            view.run_command('sublime_laravel_pint_fix')
+
+
+class ExecutableNotFoundException(BaseException):
+    pass
